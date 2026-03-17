21291c4274db0fbad83c4dc58dbf89f4f18d5cff
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-03-17 22:28:45 +0530

    Update single port.v

--- Projects/RAM/Single port Ram/single port.v
+++ Projects/RAM/Single port Ram/single port.v
@@ -1,1 +1,24 @@
 
+module ram(clk, rst, we, din, addr, dout);
+  input clk, rst, we;
+  input [7:0] din;
+  input [3:0] addr;
+  output reg [7:0] dout;
+
+  reg [7:0] mem[0:15];
+
+  always @(posedge clk)
+  begin
+    if(rst)
+    begin
+      dout <= 8'd0;
+    end
+    else
+    begin
+      if(we)
+        mem[addr] <= din;
+      else
+        dout <= mem[addr];
+    end
+  end
+endmodule
