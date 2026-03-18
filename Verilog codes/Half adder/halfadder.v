1d59e0e20f7f7efa8096590bcff4a996f9455a4c
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-03-18 10:29:12 +0530

    Update halfadder.v

--- Verilog codes/Half adder/halfadder.v
+++ Verilog codes/Half adder/halfadder.v
@@ -1,1 +1,11 @@
+module half_adder (
+    input A,
+    input B,
+    output Sum,
+    output Carry
+);
 
+assign Sum = A ^ B;     // XOR operation
+assign Carry = A & B;   // AND operation
+
+endmodule
