b1d3711d6091964180b10d684b7d70703f2237c1
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-03-18 11:09:31 +0530

    Update up counter.v

--- Verilog codes/Counters/Up counter/up counter.v
+++ Verilog codes/Counters/Up counter/up counter.v
@@ -1,1 +1,13 @@
 
+module upcounter (clk, rst, count);
+input clk, rst;
+output reg [2:0] count;
+
+always @(posedge clk)
+begin
+    if (rst)
+        count <= 3'b000;
+    else
+        count <= count + 1;
+end
+endmodule
