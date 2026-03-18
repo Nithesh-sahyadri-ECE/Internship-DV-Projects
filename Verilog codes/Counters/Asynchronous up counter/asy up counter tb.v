7520a05e122c7939d6eae49c1bbc6e056d9162b8
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-03-18 11:24:03 +0530

    Update asy up counter tb.v

--- Verilog codes/Counters/Asynchronous up counter/asy up counter tb.v
+++ Verilog codes/Counters/Asynchronous up counter/asy up counter tb.v
@@ -1,1 +1,30 @@
 
+module upcounter_tb;
+
+reg clk = 0, rst;
+wire [2:0] count;
+integer i;
+
+// Instantiate DUT
+upcounter uut (clk, rst, count);
+
+// Clock generation
+always #5 clk = ~clk;
+
+initial
+begin
+    rst = 1;
+    #10;
+    rst = 0;
+    #100;
+
+    $finish;
+end
+
+// Display values at every clock edge
+always @(posedge clk)
+begin
+    $display("clk=%b rst=%b count=%b", clk, rst, count);
+end
+
+endmodule
