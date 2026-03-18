c4659aaeb9a4778b3ef8ab257a6a4b37fb1701e5
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-03-18 11:24:33 +0530

    Add asynchronous up counter module

--- Verilog codes/Counters/Asynchronous up counter/asy up counter.v
+++ Verilog codes/Counters/Asynchronous up counter/asy up counter.v
@@ -1,1 +1,15 @@
 
+module upcounter (clk, rst, count);
+
+input clk, rst;
+output reg [2:0] count;
+
+always @(posedge clk)
+begin
+    if (rst)
+        count <= 3'b000;   // Reset to 0
+    else
+        count <= count + 1; // Increment
+end
+
+endmodule
