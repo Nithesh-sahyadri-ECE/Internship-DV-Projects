1288ce5ad9c2d1a318bd1cf69e51f1341272aa41
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-03-18 10:36:51 +0530

    Create testbench for 2-to-1 MUX
    Added a testbench for the 2-to-1 multiplexer to verify functionality.

--- Verilog codes/MUX/2 TO 1 MUX/mux tb.v
+++ Verilog codes/MUX/2 TO 1 MUX/mux tb.v
@@ -1,1 +1,34 @@
 
+module tb_mux2to1;
+
+reg I0, I1, S;
+wire Y;
+
+// Instantiate MUX
+mux2to1 uut (
+    .I0(I0),
+    .I1(I1),
+    .S(S),
+    .Y(Y)
+);
+
+initial begin
+    $display("I0 I1 S | Y");
+    $display("-----------");
+
+    I0=0; I1=0; S=0; #10;
+    $display("%b  %b  %b | %b", I0, I1, S, Y);
+
+    I0=0; I1=1; S=0; #10;
+    $display("%b  %b  %b | %b", I0, I1, S, Y);
+
+    I0=1; I1=0; S=1; #10;
+    $display("%b  %b  %b | %b", I0, I1, S, Y);
+
+    I0=1; I1=1; S=1; #10;
+    $display("%b  %b  %b | %b", I0, I1, S, Y);
+
+    $finish;
+end
+
+endmodule
