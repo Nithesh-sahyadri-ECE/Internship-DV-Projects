ad47b53935a01672e0e9181a783f492b5e093fc2
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-06-07 22:54:11 +0530

    Add testbench for D Flip-Flop module

--- System Verilog/D FlipFlop/D FF tb.v
+++ System Verilog/D FlipFlop/D FF tb.v
@@ -1,1 +1,23 @@
 
+module tb;
+
+dff_if vif();
+
+dff dut(
+  .clk(vif.clk),
+  .rst(vif.rst),
+  .d(vif.d),
+  .q(vif.q));
+
+initial begin
+vif.clk=0;
+forever #5 vif.clk=~vif.clk;
+end
+
+env e;
+initial begin
+e=new(vif);
+e.run();
+#200 $finish;
+end
+endmodule
