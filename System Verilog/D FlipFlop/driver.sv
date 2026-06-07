624f4de40224685c0e9b375812801803e6d5d6fa
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-06-07 23:04:11 +0530

    Add driver class for D Flip-Flop simulation

--- System Verilog/D FlipFlop/driver.sv
+++ System Verilog/D FlipFlop/driver.sv
@@ -1,1 +1,22 @@
 
+class driver;
+  virtual dff_if vif;
+  mailbox #(trans) mbx;
+  function new(virtual dff_if vif,mailbox #(trans) mbx);
+  this.vif=vif;
+  this.mbx=mbx;
+  endfunction
+  
+  task run();
+  repeat(15) begin
+  trans t;
+  mbx.get(t);
+  @(posedge vif.clk)
+  
+  vif.rst=t.rst;
+  vif.d =t.d;
+  
+  #1;
+  end
+  endtask
+  endclass
