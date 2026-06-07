540e6b560a3446eefd892154241ff735826942ac
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-06-07 22:52:58 +0530

    Update DFF.v

--- System Verilog/D FlipFlop/DFF.v
+++ System Verilog/D FlipFlop/DFF.v
@@ -1,1 +1,14 @@
 
+module dff(
+    input logic clk, rst, d,
+    output logic q
+);
+
+always_ff @(posedge clk or posedge rst) begin
+    if (rst)
+        q <= 1'b0;
+    else
+        q <= d;
+end
+
+endmodule
