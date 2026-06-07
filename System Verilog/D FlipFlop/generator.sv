c438d65aad394f95b581059cd57d816d5d699e7a
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-06-07 23:06:25 +0530

    Add generator class for transaction generation

--- System Verilog/D FlipFlop/generator.sv
+++ System Verilog/D FlipFlop/generator.sv
@@ -1,1 +1,17 @@
 
+class generator;
+  mailbox #(trans) mbx;
+  
+  function new(mailbox #(trans) mbx);
+  this.mbx=mbx;
+  endfunction
+  
+  task run();
+  repeat(15) begin
+  trans t=new();
+  assert(t.randomize());
+  mbx.put(t);
+  #10;
+  end
+  endtask
+endclass
