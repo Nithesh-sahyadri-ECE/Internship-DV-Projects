c41e623cb37aa8992b1691865f6271f455dc42fd
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-06-07 23:05:31 +0530

    Update environment.sv

--- System Verilog/D FlipFlop/environment.sv
+++ System Verilog/D FlipFlop/environment.sv
@@ -1,1 +1,25 @@
+class env;
+  agent agt;
+  scoreboard scb;
+  
+  mailbox #(trans) mon2scb;
+  
+  virtual dff_if vif;
+  
+  function new(virtual dff_if vif);
+  this.vif=vif;
+  
+  mon2scb = new();
+  
+  agt=new(vif,mon2scb);
+  scb=new(mon2scb);
+  endfunction
+  
+  task run;
+  fork
+  agt.run();
+  scb.run();
+  join_any
+  endtask
+endclass
 
