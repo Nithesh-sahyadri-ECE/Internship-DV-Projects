aaf9384b7ee8cd50291e387b91bdfba5e5d6766d
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-06-07 23:08:30 +0530

    Add scoreboard class for D FlipFlop verification

--- System Verilog/D FlipFlop/scoreboard.sv
+++ System Verilog/D FlipFlop/scoreboard.sv
@@ -1,1 +1,25 @@
 
+class scoreboard;
+  mailbox #(trans) mbx;
+  function new(mailbox #(trans) mbx);
+  this.mbx=mbx;
+  endfunction
+  
+  task run();
+  trans t;
+  bit exp;
+  
+  repeat(15) begin
+  mbx.get(t);
+  if(t.rst)
+  exp=0;
+  else
+  exp=t.d;
+  
+  if(exp==t.q)
+  $display("pass: rst=%0d  d=%0d  q=%0d || exp=%0d",t.rst,t.d,t.q,exp);
+  else
+  $display("fail: rst=%0d  d=%0d  q!=%0d || exp=%0d",t.rst,t.d,t.q,exp);
+  end
+  endtask
+endclass
