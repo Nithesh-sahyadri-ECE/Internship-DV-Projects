a005328fb46590e64ec5db877c7ee2731ef27e42
Nithesh-sahyadri-ECE <nithesh.ec22@sahyadri.edu.in>
2026-06-07 23:07:51 +0530

    Update monitor.sv

--- System Verilog/D FlipFlop/monitor.sv
+++ System Verilog/D FlipFlop/monitor.sv
@@ -1,1 +1,21 @@
 
+class monitor;
+  virtual dff_if vif;
+  mailbox #(trans) mbx;
+  function new(virtual dff_if vif,mailbox #(trans) mbx);
+  this.vif=vif;
+  this.mbx=mbx;
+  endfunction
+  task run();
+  repeat(15) begin
+  trans t=new();
+  #1;
+  @(posedge vif.clk)
+  t.rst=vif.rst;
+  t.d=vif.d;
+  t.q=vif.q;
+  
+  mbx.put(t);
+  end
+  endtask
+endclass
